fc7b67f1142e4f19567e2480a1171de13c8c9704
“k-usui221“ <kengo_usui@upcross.jp>
2021-09-03 18:04:08 +0900

    bladeレイアウトの作成まで

--- backend/resources/views/hello/index.blade.php
+++ backend/resources/views/hello/index.blade.php
@@ -1,26 +1,17 @@
-<html>
-<head>
-    <title>Hello/Index</title>
-    <style>
-        body {font-size:16pt; color:#999;}
-        h1 {    font-size:50pt; text-align:right; color:#f6f6f6;
-            margin:-20px 0px -30px 0px; letter-spacing:-4pt;}
-    </style>
-</head>
-<body>
-    <h1>Blade/Index</h1>
-    
-    <p>&#064;forディレクティブの例</p>
-    @foreach($cats as $cat)
-        @if($loop->first)
-            <p>※データ一覧</p><ul>
-            <p>飼っている猫の数は{{$loop->count}}匹です</p><ul>
-        @endif
-        <li>No.{{$loop->iteration}} = {{$cat}}</li>
-        @if($loop->last)
-        </ul><p>-----ここまで</p>
-        @endif
-    @endforeach    
+@extends('layouts.helloapp')
 
-</body>
-</html>
+@section('title','Index')
+
+@section('menubar')
+    @parent
+    インデックスページ
+@endsection
+
+@section('content')
+    <p>ここが本文です</p>
+    <p>必要なだけ記述できます。</p>
+@endsection
+
+@section('footer')
+copyright 2021 katou.
+@endsection
